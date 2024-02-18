e6208dba5e81429dc74b7644d5d15b72638c6dba
FloatSheep <sheep@hesiy.cn>
2024-02-18 22:05:12 +0800

    ✨ 添加 404 页面

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,30 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
+const rewriteSlashToIndexHtml = () => {
+  return {
+    name: 'rewrite-slash-to-index-html',
+    apply: 'serve',
+    enforce: 'post',
+    configureServer(server) {
+      // rewrite / as index.html
+      server.middlewares.use('/', (req, _, next) => {
+        if (req.url === '/') {
+          req.url = '/index.html'
+        }
+        next()
+      })
+    },
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), rewriteSlashToIndexHtml()],
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer]
     }
-  }
+  },
+  appType: 'mpa'
 })
